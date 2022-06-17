cb56a99a3b393e4a36b4c15def31781c5201ab20
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2022-06-17 17:35:59 +0500

    Logout button in MainActivity done.

--- app/src/main/java/com/example/udemyclone/MainActivity.kt
+++ app/src/main/java/com/example/udemyclone/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.udemyclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.udemyclone.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    //! Menu creation
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_activity_menu, menu)
+        return true
+    }
+
+    //! on menu selection
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.mLogout) {
+            Toast.makeText(this,"Logged Out!", Toast.LENGTH_SHORT).show()
+            mAuth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
