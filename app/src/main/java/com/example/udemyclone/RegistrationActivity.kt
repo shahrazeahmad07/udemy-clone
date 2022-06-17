32b0b28d37cc7fa363bbced8be1f95a78280d6ea
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2022-06-17 12:17:01 +0500

    Enabled Binding

--- app/src/main/java/com/example/udemyclone/RegistrationActivity.kt
+++ app/src/main/java/com/example/udemyclone/RegistrationActivity.kt
@@ -2,10 +2,15 @@ package com.example.udemyclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.udemyclone.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+    lateinit var binding : ActivityRegistrationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
