97b570caa2f6df5235643fb724fcbfe2420f30bf
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2022-06-17 17:35:31 +0500

    Login Activity Functionality done.

--- app/src/main/java/com/example/udemyclone/LoginActivity.kt
+++ app/src/main/java/com/example/udemyclone/LoginActivity.kt
@@ -1,102 +1,61 @@
 package com.example.udemyclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.udemyclone.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-    }
-}
 
-
-/*
-class LoginActivity : AppCompatActivity() {
-    //VIEW BINDING
-    private lateinit var binding: ActivityLoginBinding
-    //action bar
-
-    //progressDialog h
-    private lateinit var progressDialog: ProgressDialog
-    //firebase auth
-    private lateinit var firebaseAuth: FirebaseAuth
-    private var email=""
-    private var password =""
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //configure progress dialog
-        progressDialog = ProgressDialog(this)
-        progressDialog.setTitle("please wait")
-        progressDialog.setMessage("Logging in---")
-        progressDialog.setCanceledOnTouchOutside(false)
-        // init firebase
-        firebaseAuth = FirebaseAuth.getInstance()
-        checkUser()
-//handle register activity,open signup
-        binding.noAccountTv.setOnClickListener{
-            startActivity(Intent(this, SignupActivity::class.java))
-        }
-//handle click, begin login
-        binding.loginBtn.setOnClickListener{
-            validateData()
+        mAuth = FirebaseAuth.getInstance()
+
+        //! Register here
+        binding.tvLoginHere.setOnClickListener{
+            startActivity(Intent(this, RegistrationActivity::class.java))
         }
 
-    }
-    private fun validateData(){
-
-        email=binding.emailEt.text.toString().trim()
-        password=binding.passwordEt.text.toString().trim()
-        //validate data
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            binding.emailEt.error="invalid email format"
-
-        }
-        else if (TextUtils.isEmpty(password)){
-            binding.passwordEt.error="please enter password"
-        }
-        else
-        {
-            firebaseLogin()
-        }
-    }
-    private fun firebaseLogin(){
-        //show progress
-        progressDialog.show()
-        firebaseAuth.signInWithEmailAndPassword(email, password)
-            .addOnSuccessListener {
-                //login success
-                progressDialog.dismiss()
-                val firebaseUser= firebaseAuth.currentUser
-                val email = firebaseUser!!.email
-                Toast.makeText(this, "LOGIN  Successfully as $email", Toast.LENGTH_SHORT).show()
-                //open profile
-
-                startActivity(Intent(this,ProfileActivity::class.java))
-                finish()
+        binding.btnLogin.setOnClickListener {
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+            if (username.isEmpty() && password.isEmpty()) {
+                Toast.makeText(this, "Required fields empty..", Toast.LENGTH_SHORT).show()
             }
-            .addOnFailureListener { e->
-                //login failed
-                progressDialog.dismiss()
-                Toast.makeText(this, "LOGIN FAILED DUE TO ${e.message}", Toast.LENGTH_SHORT).show()
-
+            else {
+                binding.progressBar.visibility = View.VISIBLE
+                mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        binding.progressBar.visibility = View.GONE
+                        Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }
+                    else {
+                        binding.progressBar.visibility = View.GONE
+                        Toast.makeText(this, "Login Failed..", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
+        }
     }
 
-    private fun checkUser() {
-        // if user is already login to activity .get current user
-        val firebaseUser= firebaseAuth.currentUser
-        if(firebaseUser != null)
-        {
-            startActivity(Intent(this, ProfileActivity::class.java) )
+    override fun onStart() {
+        super.onStart()
+        val user = mAuth.currentUser
+        if (user != null) {
+            startActivity(Intent(this,MainActivity::class.java))
             finish()
-
         }
     }
-
-
- */
+}
--- app/src/main/java/com/example/udemyclone/RegistrationActivity.kt
+++ app/src/main/java/com/example/udemyclone/RegistrationActivity.kt
@@ -1,16 +1,55 @@
 package com.example.udemyclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.udemyclone.databinding.ActivityRegistrationBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistrationActivity : AppCompatActivity() {
-    lateinit var binding : ActivityRegistrationBinding
+    private lateinit var binding : ActivityRegistrationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //! Already a user Functionality
+        binding.tvLoginHere.setOnClickListener {
+            finish()
+        }
+
+        //! Register Button
+        binding.btnRegister.setOnClickListener {
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+            val confirmPassword = binding.etConfirmPassword.text.toString()
+
+            if (username.isEmpty() && password.isEmpty() && confirmPassword.isEmpty()) {
+                Toast.makeText(this, "Required fields empty..", Toast.LENGTH_SHORT).show()
+            }
+            else if (password != confirmPassword) {
+                Toast.makeText(this, "Password doesn't match", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                binding.progressBar.visibility = View.VISIBLE
+                val mAuth = FirebaseAuth.getInstance()
+                mAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener {
+                    if(it.isSuccessful) {
+                    binding.progressBar.visibility = View.GONE
+                        Toast.makeText(this,"User Registered!", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }
+                    else {
+                        binding.progressBar.visibility = View.GONE
+                        Toast.makeText(this, "Fail to Register User..", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
