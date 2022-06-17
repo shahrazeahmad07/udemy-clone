af498165ce9479c74a1474216e31535e7a69ccae
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2022-06-17 11:58:02 +0500

    RegistrationActivity UI Complete

---
+++ app/src/main/java/com/example/udemyclone/AddCourseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.udemyclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCourseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_course)
+    }
+}
